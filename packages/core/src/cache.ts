e47975106b1476610c277d74acb11dbe3b3d5142
interc0der <intercoder@whirled.io>
2023-02-08 17:06:33 -0800

    Update: latest updates

---
+++ packages/core/src/cache.ts
@@ -0,0 +1,41 @@
+import { defaultLogger, Logger } from './logger';
+import { Payload } from './payload';
+
+export interface PayloadCacheConfig {
+  onError?: (error: unknown, payload: Payload<unknown, unknown, unknown>) => void;
+  onSuccess?: (data: unknown, payload: Payload<unknown, unknown, unknown>) => void;
+}
+
+export const DefaultPayloadCacheConfig: PayloadCacheConfig = {
+  onError: (error: unknown, payload: Payload<unknown, unknown, unknown>) => {},
+  onSuccess: (error: unknown, payload: Payload<unknown, unknown, unknown>) => {},
+};
+
+export interface DefaultOptions {}
+export const defaultPayloadOptions = {};
+
+export class PayloadCache {
+  private logger: Logger;
+  private options: DefaultOptions;
+  private mountCount: number;
+  private payloads: [];
+  private payloadsMap: DefaultOptions;
+  private config: PayloadCacheConfig;
+
+  constructor(config?: PayloadCacheConfig) {
+    this.options = defaultPayloadOptions;
+    this.logger = defaultLogger;
+    this.config = config || DefaultPayloadCacheConfig;
+    this.payloads = [];
+    this.payloadsMap = {};
+    this.mountCount = 0;
+
+    this.mount();
+  }
+
+  public mount() {
+    this.mountCount++;
+    if (this.mountCount !== 1) return;
+  }
+  public unmount() {}
+}
